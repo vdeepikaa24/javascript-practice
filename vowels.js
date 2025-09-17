3e19a753c5b14f16727616f7153b5d519ed856ff
vdeepikaa24 <vdeepikaa24@gmail.com>
2025-09-17 09:09:38 +0530

    z

--- vowels.js
+++ vowels.js
@@ -1,11 +1,12 @@
-const countVowels = (str) => {
-    const vowels = "aeiouAEIOU";
-    let count = 0;
-    str.split("").map(char => {
-        if(vowels.includes(char)) {
-            count++;
-        }
-    });
-    return count;
-};
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  str.split("").map(function (char) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  });
+  return count;
+}
+
 console.log(countVowels("Practice makes a man perfect"));
