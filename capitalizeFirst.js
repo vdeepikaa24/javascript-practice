1c6d9da1003394520bc696e9d96852dcd29dd6f1
vdeepikaa24 <vdeepikaa24@gmail.com>
2025-09-17 08:53:24 +0530

    Capitalize

--- capitalizeFirst.js
+++ capitalizeFirst.js
@@ -1,7 +1,10 @@
 function capitalizeWords(sentence) {
   return sentence
     .split(" ")
-    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .map(function (word) {
+      return word[0].toUpperCase() + word.slice(1);
+    })
     .join(" ");
 }
-console.log(capitalizeWords("practice makes man perfect"));
+
+console.log(capitalizeWords("practice makes a man perfect"));
