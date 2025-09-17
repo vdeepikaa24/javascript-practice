1e35e1e735a66d3565f3c04dccebb5962a1c4bb3
vdeepikaa24 <vdeepikaa24@gmail.com>
2025-09-17 09:06:54 +0530

    Longest word in a sentence

--- longestWord.js
+++ longestWord.js
@@ -1,11 +1,12 @@
-const longestWord = (sentence) => {
+function longestWord(sentence) {
     let words = sentence.split(" ");
     let longest = "";
-    words.map(word => {
+    words.map(function(word) {
         if (word.length > longest.length) {
             longest = word;
         }
     });
     return longest;
-};
+}
+
 console.log(longestWord("Practice makes a man perfect"));
